a3a63a16bbd5b03965cc3b7b15e1a314d3af80c6
merkede <123862480+merkede@users.noreply.github.com>
2023-08-11 21:30:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ plant = load_url("https://lottie.host/317802e0-e0a9-4715-9eaf-6ef6511ea52d/vvXUH
 farmer = load_url("https://lottie.host/317802e0-e0a9-4715-9eaf-6ef6511ea52d/vvXUHRFJ2L.json")
 
 
-
 # --- Load The Model ---
 recommender = pickle.load(open('CropRecommender.sav', 'rb'))
 
@@ -37,8 +36,7 @@ def main():
         c1, c2 = st.columns((1, 1.5))
         with c1:
             st.title("Tariff Recommender")
-            st.subheader("Let's find you our best tariff:")
-
+            st.write("Let's find you the best tariff:")
 
         with c2:
             st_lottie(farmer, height=650, key="farmer")
@@ -46,18 +44,21 @@ def main():
     with st.container():
         col1, col2 = st.columns((1, 1))
         with col1:
+            st.header("Have Fun 😄")
             # -- Time to take user input --
             # Our model takes 7 parameters so we need 7 input fields
 
             n = st.number_input("Number of bedrooms", min_value=0, max_value=100)
-            
-            # Change these input fields to use radio buttons for "Yes" or "No" responses
             p = st.radio("Do you have a SMART meter?", ("No", "Yes"))
             k = st.radio("Do you have a strong preference for 100% green electricity?", ("No", "Yes"))
             temperature = st.number_input("What is your monthly consumption?", min_value=0, max_value=100)
             humidity = st.radio("Are you an existing BG customer?", ("No", "Yes"))
             ph_value = st.number_input("What bundle offers would interest you?", min_value=0, max_value=15)
             rainfall = st.radio("Would you want to join BG Peaksave Sunday?", ("No", "Yes"))
+            
+            # Additional question for existing BG customers
+            if humidity == "Yes":
+                bg_customer_reference = st.text_input("Please enter your BG customer reference:")
 
             # --- Code for recommendation ---
             crop_output = ""
@@ -69,7 +70,12 @@ def main():
                 humidity_numeric = 1 if humidity == "Yes" else 0
                 rainfall_numeric = 1 if rainfall == "Yes" else 0
                 
-                crop_output = crs_output([n, p_numeric, k_numeric, temperature, humidity_numeric, ph_value, rainfall_numeric])
+                # Include the BG customer reference in the input data if provided
+                input_data = [n, p_numeric, k_numeric, temperature, humidity_numeric, ph_value, rainfall_numeric]
+                if humidity == "Yes":
+                    input_data.append(bg_customer_reference)
+
+                crop_output = crs_output(input_data)
                 with col2:
                     st_lottie(plant, height=800, key="plant")
 
@@ -77,3 +83,4 @@ def main():
 
 # if __name__ == '__main__':
 main()
+
