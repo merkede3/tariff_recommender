c5dafa0afad4153043ef260a04f0939b4a89b6fb
merkede3 <125595060+merkede3@users.noreply.github.com>
2023-08-16 21:50:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
-'''
-st.set_page_config(page_title="Crop Recommender System", layout="wide")
+
+st.set_page_config(page_title="Tariff Recommender System", layout="wide")
 
 # --- Important Functions ---
 def load_url(url):
